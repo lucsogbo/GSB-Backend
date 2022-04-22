c6b884b4cf7e2c010974425230d98d3c77d915e5
luc_sogbo <sogbosedjroluc@gmail.com>
2022-04-22 23:45:54 +0100

    first commit

---
+++ src/main/java/bj/dgi/GSBBackend/GsbBackendApplication.java
@@ -0,0 +1,18 @@
+package bj.dgi.GSBBackend;
+
+import bj.dgi.GSBBackend.entities.FileStorageProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
+public class GsbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsbBackendApplication.class, args);
+	}
+
+}
